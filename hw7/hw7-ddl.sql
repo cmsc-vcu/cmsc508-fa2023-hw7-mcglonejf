463155245ab76460396bc80ee496bce58621613d
James McGlone <mcglonejf@vcu.edu>
2023-11-16 00:58:02 -0500

    completed hw7

--- hw7/hw7-ddl.sql
+++ hw7/hw7-ddl.sql
@@ -17,7 +17,11 @@
 # Drops all tables.  This section should be amended as new tables are added.
 
 SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS peopleroles;
+DROP TABLE IF EXISTS peopleskills;
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS roles;
 # ... 
 SET FOREIGN_KEY_CHECKS=1;
 
@@ -28,11 +32,35 @@ SET FOREIGN_KEY_CHECKS=1;
 # time committment offers some sense of how much time was required (or will be required) to gain the skill.
 # You can assign the skill descriptions.  Please be creative!
 
+drop table if exists skills;
+CREATE TABLE skills(
+    skills_id int not null,
+    skills_name varchar(255) not null,
+    skills_description varchar(255) not null default '(default description)',
+    skills_tag varchar(255) not null,
+    skills_url varchar(255),
+    skills_time_commitment int,
+    primary key (skills_id)
+);
+
+# SELECT * from skills;
+
+
 
 # Section 3
 # Populate skills
 # Populates the skills table with eight skills, their tag fields must exactly contain “Skill 1”, “Skill 2”, etc.
 # You can assign skill names.  Please be creative!
+
+INSERT INTO skills ( skills_id, skills_name, skills_description, skills_tag) values
+    (1, 'kick boxing', 'visualize this!', 'Skill 1'),
+    (2, 'python coding', 'writing python programs', 'Skill 2'),
+    (3, 'graphic design', 'creating visually appealing graphics', 'Skill 3'),
+    (4, 'data analysis', 'analyzing and interpreting data', 'Skill 4'),
+    (5, 'public speaking', 'effective communication in front of an audience', 'Skill 5'),
+    (6, 'web development', 'building and maintaining websites', 'Skill 6'),
+    (7, 'foreign language proficiency', 'fluent in a foreign language', 'Skill 7'),
+    (8, 'project management', 'leading and organizing projects', 'Skill 8');
 
 
 # Section 4
@@ -40,10 +68,18 @@ SET FOREIGN_KEY_CHECKS=1;
 # ID cannot be null, Last name cannot be null, date joined cannot be NULL.
 # All other fields can default to NULL.
 
+drop table if exists people;
 CREATE TABLE people (
-    people_id int,
-    people_last_name varchar(256) NOT NULL,
-    PRIMARY KEY (people_id)
+    people_id int not null,
+    people_last_name varchar(255) NOT NULL,
+    PRIMARY KEY (people_id),
+    people_first_name varchar(255),
+    people_email varchar(255),
+    people_linkedin_url varchar(255),
+    people_headshot_url varchar(255),
+    people_discord_handle varchar(255),
+    people_brief_bio varchar(255),
+    people_date_joined date not null
 );
 
 # Section 5
@@ -51,12 +87,38 @@ CREATE TABLE people (
 # Their last names must exactly be “Person 1”, “Person 2”, etc.
 # Other fields are for you to assign.
 
-insert into people (people_id,people_last_name) values (1,'Person 1');
+insert into people (people_id,people_last_name, people_first_name, people_date_joined) values 
+    (1, 'Person 1', 'John', '2023-01-01'),
+    (2, 'Person 2', 'Alice', '2023-02-15'),
+    (3, 'Person 3', 'Bob', '2023-03-10'),
+    (4, 'Person 4', 'Emily', '2023-04-05'),
+    (5, 'Person 5', 'Charlie', '2023-05-20'),
+    (6, 'Person 6', 'Olivia', '2023-06-15'),
+    (7, 'Person 7', 'Michael', '2023-07-01'),
+    (8, 'Person 8', 'Sara', '2023-08-12'),
+    (9, 'Person 9', 'David', '2023-09-25'),
+    (10, 'Person 10', 'Emma', '2023-10-10');
+
+
+# SELECT * from people;
+
 
 
 # Section 6
 # Create peopleskills( id, skills_id, people_id, date_acquired )
 # None of the fields can ba NULL. ID can be auto_increment.
+
+drop table if exists peopleskills;
+CREATE TABLE peopleskills(
+    id int not null auto_increment,
+    skills_id int not null,
+    people_id int not null,
+    date_acquired date not null default (current_date),
+    primary key (id),
+    foreign key (skills_id) references skills (skills_id),
+    foreign key (people_id) references people (people_id),
+    unique (skills_id,people_id)
+);
 
 
 # Section 7
@@ -72,11 +134,49 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 9 has skills 2,5,6;
 # Person 10 has skills 1,4,5;
 # Note that no one has yet acquired skills 7 and 8.
- 
+
+insert into peopleskills ( people_id,skills_id) values
+    (1,1),
+    (1,3),
+    (1, 6),
+    (2, 3),
+    (2, 4),
+    (2, 5),
+    (3, 1),
+    (3, 5),
+    (5, 3),
+    (5, 6),
+    (6, 2),
+    (6, 3),
+    (6, 4),
+    (7, 3),
+    (7, 5),
+    (7, 6),
+    (8, 1),
+    (8, 3),
+    (8, 5),
+    (8, 6),
+    (9, 2),
+    (9, 5),
+    (9, 6),
+    (10, 1),
+    (10, 4),
+    (10, 5);
+
+# SELECT * from peopleskills;
+
 
 # Section 8
 # Create roles( id, name, sort_priority )
 # sort_priority is an integer and is used to provide an order for sorting roles
+
+drop table if exists roles;
+CREATE TABLE roles(
+    id int,
+    name varchar(255),
+    sort_priority int,
+    primary key (id)
+);
 
 
 
@@ -85,11 +185,29 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Designer, Developer, Recruit, Team Lead, Boss, Mentor
 # Sort priority is assigned numerically in the order listed above (Designer=10, Developer=20, Recruit=30, etc.)
 
-
+insert into roles (id, name, sort_priority) values  
+    (1, 'Designer', 10),
+    (2, 'Developer', 20),
+    (3, 'Recruit', 30),
+    (4, 'Team Lead', 40),
+    (5, 'Boss', 50),
+    (6, 'Mentor', 60);
 
 # Section 10
 # Create peopleroles( id, people_id, role_id, date_assigned )
 # None of the fields can be null.  ID can be auto_increment
+
+drop table if exists peopleroles;
+CREATE TABLE peopleroles(
+    id int not null auto_increment,
+    people_id int not null,
+    role_id int not null,
+    date_assigned date not null default (current_date),
+    primary key (id),
+    foreign key (people_id) references people (people_id),
+    foreign key (role_id) references roles (id),
+    unique (people_id,role_id)
+);
 
 
 
@@ -105,4 +223,30 @@ insert into people (people_id,people_last_name) values (1,'Person 1');
 # Person 8 is Designer and Team Lead
 # Person 9 is Developer
 # Person 10 is Developer and Designer
+
+insert into peopleroles (people_id, role_id) values
+    (1, 2),
+    (2, 5),
+    (2, 6),
+    (3, 2),
+    (3, 4),
+    (4, 3),
+    (5, 3),
+    (6, 2),
+    (6, 1),
+    (7, 1),
+    (8, 1),
+    (8, 4),
+    (9, 2),
+    (10, 2),
+    (10, 1);
+
+# SELECT * from peopleroles;
+
+
+
+
+
+
+
 
